637bf9015fd21ddbb880de0b41e90acbf8a77122
William Decker <willdecker@gmail.com>
2020-05-03 15:42:36 -0400

    Create basic blog post template. Setup gatsby-node.js to generate blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blogPost.js")
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allMarkdownRemark.edges.forEach(post => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${post.node.fields.slug}`,
+      context: {
+        slug: post.node.fields.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/blogPost.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const BlogPost = () => {
+  return <Layout>This is the blog template</Layout>
+}
+
+export default BlogPost
