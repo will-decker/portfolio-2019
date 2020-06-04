615dfb5a6733588a54a472ba31d07bb091c50f2f
William Decker <willdecker@gmail.com>
2020-06-04 03:15:20 -0400

    Add BLM message

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,19 +2,31 @@ import React from "react"
 import avatar from "../svgs/WD_Avatar.svg"
 
 const Intro = () => (
-  <section id="intro">
-    <div className="intro-container">
-      <div className="intro-text">
-        <h1>Will Decker</h1>
-        <h2>Front-End Developer</h2>
-        {""}
-        <h2>& Web Designer</h2>
+  <>
+    <section id="blm-message">
+      <div className="message-container">
+        <h1>Black Lives Matter</h1>
+        <p>Support the movement against racism and police brutality.</p>
+        <a href="#" className="donate-btn">
+          Donate
+        </a>
       </div>
-      <div className="intro-avatar">
-        <img src={avatar} alt="Will Decker Avatar" />
+      <div className="scrolldown-msg">
+        Scroll down to continue to willdecker.com
       </div>
-    </div>
-  </section>
+    </section>
+    <section id="intro">
+      <div className="intro-container">
+        <div className="intro-text">
+          <h1>Will Decker</h1>
+          <h2>Web Developer</h2>
+        </div>
+        <div className="intro-avatar">
+          <img src={avatar} alt="Will Decker Avatar" />
+        </div>
+      </div>
+    </section>
+  </>
 )
 
 export default Intro
