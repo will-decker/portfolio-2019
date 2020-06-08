459bcf840c1d79a65e0b2a2940aa534325b3d26c
William Decker <willdecker@gmail.com>
2020-06-08 02:03:55 -0400

    Create BLM banner component

---
+++ src/components/blmbanner.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+const BLMBanner = () => (
+  <section id="blm-message-banner">
+    <div className="message-container">
+      <h1>Black Lives Matter</h1>
+      <p>
+        Support the movement against racial inequality, social injustice and
+        police brutality.
+      </p>
+      <div className="msg-btns">
+        <a
+          href="https://cutt.ly/donate-blm"
+          target="_blank"
+          className="donate-btn"
+        >
+          Donate
+        </a>
+        <a
+          href="https://cutt.ly/blacklivesmatter-home"
+          target="_blank"
+          className="donate-btn"
+        >
+          Learn More
+        </a>
+      </div>
+    </div>
+  </section>
+)
+
+export default BLMBanner
