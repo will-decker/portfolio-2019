fcc5034e34e009534c0ed21571596b780560adcd
Will Decker <willdecker@gmail.com>
2021-04-06 01:34:29 -0400

    remove photoshop and illustrator logos from skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react"
 
-import GitHub from '../images/skills/github.svg'
-import JavaScript from '../images/skills/javascript.svg'
-import ReactJS from '../images/skills/react.svg'
-import HTML5 from '../images/skills/html5.svg'
-import CSS3 from '../images/skills/css3.svg'
-import Sass from '../images/skills/sass.svg'
-import Bootstrap from '../images/skills/bootstrap.svg'
-import Photoshop from '../images/skills/photoshop.svg'
-import Illustrator from '../images/skills/illustrator.svg'
-import WordPress from '../images/skills/wordpress.svg'
+import GitHub from "../images/skills/github.svg"
+import JavaScript from "../images/skills/javascript.svg"
+import ReactJS from "../images/skills/react.svg"
+import HTML5 from "../images/skills/html5.svg"
+import CSS3 from "../images/skills/css3.svg"
+import Sass from "../images/skills/sass.svg"
+import Bootstrap from "../images/skills/bootstrap.svg"
+import WordPress from "../images/skills/wordpress.svg"
 
 const Skills = () => {
-  const logos = [HTML5, CSS3, JavaScript, ReactJS, Sass, Bootstrap, GitHub, WordPress, Photoshop, Illustrator];
+  const logos = [
+    HTML5,
+    CSS3,
+    JavaScript,
+    ReactJS,
+    Sass,
+    Bootstrap,
+    GitHub,
+    WordPress,
+  ]
   return (
     <section className="skills" data-aos="fade-right">
       <h2>Skills</h2>
@@ -26,11 +33,7 @@ const Skills = () => {
 }
 
 const Skill = ({ skill }) => (
-  <img
-    className="skill-logo skill-transition"
-    src={skill}
-    alt=""
-  />
+  <img className="skill-logo skill-transition" src={skill} alt="" />
 )
 
 export default Skills
