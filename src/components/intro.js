ece3b84d433e36c985746cb710f28c2831648b3f
Will Decker <willdecker@gmail.com>
2021-04-07 12:49:28 -0400

    remove avatar from intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,5 +1,4 @@
 import React from "react"
-import avatar from "../svgs/WD_Avatar.svg"
 
 const Intro = () => (
   <>
@@ -8,9 +7,6 @@ const Intro = () => (
         <div className="intro-text">
           <h1>Will Decker</h1>
           <h2>Web Developer</h2>
-        </div>
-        <div className="intro-avatar">
-          <img src={avatar} alt="Will Decker Avatar" />
         </div>
       </div>
     </section>
