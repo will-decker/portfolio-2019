72dbe9956d84f7da64f93f39dc3029e22281ef37
Will Decker <willdecker@gmail.com>
2021-05-18 02:32:31 -0400

    install threejs, react-three-fiber, drei, react spring for 3D intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,5 +1,20 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Link } from "gatsby"
+import * as THREE from "three"
+import { Environment } from "@react-three/drei"
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
+
+function Rig() {
+  const { camera, mouse } = useThree()
+  const vec = new THREE.Vector3()
+  return useFrame(() => {
+    camera.position.lerp(
+      vec.set(mouse.x * -5, mouse.y * -4, camera.position.z),
+      0.08
+    )
+    camera.lookAt(0, 0, 0)
+  })
+}
 
 const Intro = () => (
   <>
@@ -13,6 +28,24 @@ const Intro = () => (
           </Link>
         </div>
       </div>
+      <Canvas
+        camera={{
+          position: [0, -20, 60],
+        }}
+        // style={{ height: '50vh' }}
+      >
+        <ambientLight intensity={0.2} />
+        <pointLight position={[20, 20, 20]} intensity={0.4} />
+        <pointLight position={[-20, 20, -20]} intensity={0.4} />
+
+        {/* <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} /> */}
+        <Suspense fallback={null}>
+          <Environment preset={"sunset"} background={false} />
+          <Model />
+        </Suspense>
+        <Rig />
+      </Canvas>
     </section>
   </>
 )
